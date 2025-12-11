3a353deea181c8aa5b709801670e1ae916df511c
MinKhant-cell <mink75311@gmail.com>
2025-12-12 02:50:40 +0700

    timetables init

--- src/timetables/timetables.service.ts
+++ src/timetables/timetables.service.ts
@@ -1,15 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTimetableDto } from './dto/create-timetable.dto';
 import { UpdateTimetableDto } from './dto/update-timetable.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class TimetablesService {
+  constructor(private prismaService: PrismaService) {}
   create(createTimetableDto: CreateTimetableDto) {
     return 'This action adds a new timetable';
   }
 
-  findAll() {
-    return `This action returns all timetables`;
+   async getAll() {
+    return await this.prismaService.timetable.findMany();
+  }
+async findAll(params: {
+    page?: number;
+    limit?: number;
+    sortBy?: string;
+    sortOrder?: 'asc' | 'desc';
+    search?: string;
+  }) {
+    const {
+      page = 1,
+      limit = 10,
+      sortBy = 'id',
+      sortOrder = 'desc',
+      search
+    } = params || {};
+    const where: any = {};  
+
+    if (search) {
+        where.OR = [
+          {name: { contains: search}},
+        ];
+    }
+
+    const skip = (page - 1) * limit;
+    const orderBy = { [sortBy]: sortOrder };
+
+    const [data, total] = await Promise.all([
+      this.prismaService.timetable.findMany({
+        where,
+        skip,
+        take: limit,
+        orderBy,
+      }),
+      this.prismaService.timetable.count({ where }),
+    ]);
+
+    return {
+      data,
+      meta: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   }
 
   findOne(id: number) {
